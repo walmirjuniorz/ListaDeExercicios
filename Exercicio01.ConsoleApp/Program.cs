2b60a20e8b2050f3ca1ba1af8a41039a2fbb53b7
Walmir Junior <junioroliveira8100@gmail.com>
2026-04-02 19:44:42 -0300

    Adicionada exercicio 01

--- Exercicio01.ConsoleApp/Program.cs
+++ Exercicio01.ConsoleApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Crie um programa para calcular o volume de uma caixa retangular
+
+// Multiplique o comprimento, a largura e a altura 
+// (Fórmula: V = C . L . A) Caixa: 50 cm (comprimento) x 30 cm (largura) x 20 cm (altura).
+
+Console.Write("Digite o comprimento da caixa retangular: ");
+double comprimento = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite a largura da caixa retangular: ");
+double largura = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite a altura da caixa retangular: ");
+double altura = Convert.ToDouble(Console.ReadLine());
+
+double volume = comprimento * largura * altura;
+
+Console.WriteLine($"O volume da caixa é: {volume:F2} cm");
+
+Console.ReadLine();
