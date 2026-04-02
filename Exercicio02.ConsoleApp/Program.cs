660fa244d4112a4d7eb49fbba576b430c9806683
Walmir Junior <junioroliveira8100@gmail.com>
2026-04-02 19:59:46 -0300

    Adicioando Exercicio 02

---
+++ Exercicio02.ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+2. Crie um programa que calcule o consumo de combustível por quilômetro percorrido em uma
+viagem.
+    O programa deve solicitar ao usuário:
+        a. A quilometragem inicial do veículo no início da viagem.
+        b. A quilometragem final ao término da viagem.
+        c. A quantidade de combustível consumida durante a viagem (em litros).
+*/
+
