5940f5a3f632df6f7216ecdd2b28f19c0a9a532a
Walmir Junior <junioroliveira8100@gmail.com>
2026-04-02 20:52:09 -0300

    Finalizado Exercicio 02

--- Exercicio02.ConsoleApp/Program.cs
+++ Exercicio02.ConsoleApp/Program.cs
@@ -7,3 +7,16 @@ viagem.
         c. A quantidade de combustível consumida durante a viagem (em litros).
 */
 
+Console.Write("Qual o KM inicial?: ");
+double kmInicial = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Qual o KM Final?: ");
+double kmFinal = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Quantos litros de combústivel foram consumidos?: ");
+double litrosConsumidos = Convert.ToDouble(Console.ReadLine());
+
+double distancia = kmFinal - kmInicial;
+double resultado = distancia / litrosConsumidos;
+
+Console.Write($"o consumo foi de: {resultado} litros");
