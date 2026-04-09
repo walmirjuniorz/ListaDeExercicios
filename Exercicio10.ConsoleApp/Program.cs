e91417590a2b036871e45c1d386be4e91d7d2b69
Walmir Junior <junioroliveira8100@gmail.com>
2026-04-09 19:46:37 -0300

    Exercicio 10

--- Exercicio10.ConsoleApp/Program.cs
+++ Exercicio10.ConsoleApp/Program.cs
@@ -1,7 +1,7 @@
-﻿/*
-Escreva um programa que imprima os números de 1 a 100 em ordem crescente,
-substituindo os números múltiplos de 3 pela palavra “Fizz”; e os múltiplos de 5 pela palavra “Buzz”. 
-Para números que são múltiplos de ambos, use “FizzBuzz”.
+﻿/*  Exercicio 10
+    Escreva um programa que imprima os números de 1 a 100 em ordem crescente,
+    substituindo os números múltiplos de 3 pela palavra “Fizz”; e os múltiplos de 5 pela palavra “Buzz”. 
+    Para números que são múltiplos de ambos, use “FizzBuzz”.
 */
 
 for (int i = 1; i <= 100; i++)
@@ -20,6 +20,6 @@ for (int i = 1; i <= 100; i++)
     }
     else
     {
-        Console.Write(Convert.ToString(i) + "");
+        Console.Write(Convert.ToString(i) + " ");
     }
 }
