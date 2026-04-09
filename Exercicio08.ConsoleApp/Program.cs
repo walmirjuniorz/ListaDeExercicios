eb8b0c67ccfcb8b9d7b21392ef361b8b9a6cdd37
Walmir Junior <junioroliveira8100@gmail.com>
2026-04-09 19:02:14 -0300

    Finalizado Exercicio 07

---
+++ Exercicio08.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿//Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar.
+
+
+Console.Write("Digite o número: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine("Seu número é Par.");
+}
+else
+{
+    Console.WriteLine("Seu número é Ímpar.");
+}
