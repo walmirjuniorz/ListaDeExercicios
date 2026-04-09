550234a09661598c58b8784020e422cbd7e35745
Walmir Junior <junioroliveira8100@gmail.com>
2026-04-09 19:19:12 -0300

    Exercicio 04

--- Exercicio04.ConsoleApp/Program.cs
+++ Exercicio04.ConsoleApp/Program.cs
@@ -1,7 +1,7 @@
-﻿/*
-4. Crie um programa para calcular o salário total de um vendedor.
-Deverá ser informado o salário base e o total de vendas. 
-A comissão é calculada com um percentual (informado pelo usuário) sobre o total de vendas.
+﻿/* Exercicio 04
+    4. Crie um programa para calcular o salário total de um vendedor.
+    Deverá ser informado o salário base e o total de vendas. 
+    A comissão é calculada com um percentual (informado pelo usuário) sobre o total de vendas.
 */
 
 Console.Write("Informe o salário base: ");
