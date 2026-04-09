1715a69939f1dab11e4f73bd8845f19f93971b18
Walmir Junior <junioroliveira8100@gmail.com>
2026-04-09 19:14:22 -0300

    Finalizado Exercicio 09

---
+++ Exercicio09.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Escreva um algoritmo que leia um valor inicial A e imprima a sequência de
+valores do cálculo de A! e o seu resultado.
+    a. Ex: 5! = 5 X 4 X 3 X 2 X 1 = 120
+    b. Pesquise sobre “fatorial” 
+*/
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.Write("FizzBuzz ");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.Write("Fizz ");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.Write("Buzz ");
+    }
+    else
+    {
+        Console.Write(Convert.ToString(i) + "");
+    }
+}
---
+++ Exercicio10.ConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿/*
+Escreva um programa que imprima os números de 1 a 100 em ordem crescente,
+substituindo os números múltiplos de 3 pela palavra “Fizz”; e os múltiplos de 5 pela palavra “Buzz”. 
+Para números que são múltiplos de ambos, use “FizzBuzz”.
+*/
+
